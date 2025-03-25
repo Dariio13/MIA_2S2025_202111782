4e00ca755c4b1a1cbc5188c56d8779b0c58ed87d
JavierB20 <josejavierb10@gmail.com>
2025-03-25 16:13:10 -0600

    Comando varios/reinicio repo problemas con linux

---
+++ README.md
@@ -0,0 +1,3 @@
+### MIA Proyecto 1
+### Nombre: Jose Javier Bonilla Salazar
+### Carnet: 202200035
---
+++ backend/analyzer/analyzer.go
@@ -0,0 +1,33 @@
+package analyzer
+
+import (
+	commands "backend/commands"
+	"errors"  
+	"fmt"     
+	"strings" 
+)
+
+func Analyzer(input string) (string, error) {
+	tokens := strings.Fields(input)
+
+	if len(tokens) == 0 {
+		return "", errors.New("no se proporcionó ningún comando")
+	}
+
+	switch tokens[0] {
+	case "mkdisk":
+		return commands.ParseMkdisk(tokens[1:])
+	case "fdisk":
+		return commands.ParseFdisk(tokens[1:])
+	case "mount":
+		return commands.ParseMount(tokens[1:])
+	case "mkfs":
+		return commands.ParseMkfs(tokens[1:])
+	case "rep":
+		return commands.ParseRep(tokens[1:])
+	case "mkdir":
+		return commands.ParseMkdir(tokens[1:])
+	default:
+		return "", fmt.Errorf("comando desconocido: %s", tokens[0])
+	}
+}
